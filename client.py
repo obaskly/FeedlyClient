220428e72eb426777136cb06225f09858f266c73
Bropocalypse Team <amineninja9@gmail.com>
2023-05-20 02:39:16 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,108 +1,91 @@
-# -*- coding: UTF-8 -*-
 import requests
 import json
 
-class FeedlyClient(object):
-    def __init__(self, **options):
-        self.client_id = options.get('client_id')
-        self.client_secret = options.get('client_secret')
-        self.sandbox = options.get('sandbox', True)
-        if self.sandbox:
-            default_service_host = 'sandbox.feedly.com'
-        else:
-            default_service_host = 'cloud.feedly.com'
-        self.service_host = options.get('service_host', default_service_host)
-        self.additional_headers = options.get('additional_headers', {})
-        self.token = options.get('token')
-        self.secret = options.get('secret')
+class FeedlyClient:
+    def __init__(self, sandbox=True, additional_headers=None):
+        self.client_id = 'FEEDLY_CLIENT_ID'
+        self.client_secret = 'FEEDLY_CLIENT_SECRET'
+        self.token = 'FEEDLY_API_TOKEN'
+        self.sandbox = sandbox
+        self.service_host = 'sandbox.feedly.com' if sandbox else 'cloud.feedly.com'
+        self.additional_headers = additional_headers or {}
+
+    def _get_endpoint(self, path=None):
+        url = f"https://{self.service_host}"
+        if path is not None:
+            url += f"/{path}"
+        return url
+
+    def _make_request(self, method, endpoint, params=None, data=None, headers=None):
+        url = self._get_endpoint(endpoint)
+        headers = headers or {}
+        if self.token:
+            headers['Authorization'] = f'OAuth {self.token}'
+
+        response = requests.request(method, url, params=params, json=data, headers=headers)
+        response.raise_for_status()
+
+        return response.json()
 
     def get_code_url(self, callback_url):
         scope = 'https://cloud.feedly.com/subscriptions'
         response_type = 'code'
         
-        request_url = '%s?client_id=%s&redirect_uri=%s&scope=%s&response_type=%s' % (
-            self._get_endpoint('v3/auth/auth'),
-            self.client_id,
-            callback_url,
-            scope,
-            response_type
-            )        
+        request_url = f"{self._get_endpoint('v3/auth/auth')}?client_id={self.client_id}&redirect_uri={callback_url}&scope={scope}&response_type={response_type}"      
         return request_url
     
-    def get_access_token(self,redirect_uri,code):
-        params = dict(
-                      client_id=self.client_id,
-                      client_secret=self.client_secret,
-                      grant_type='authorization_code',
-                      redirect_uri=redirect_uri,
-                      code=code
-                      )
-        
-        quest_url=self._get_endpoint('v3/auth/token')
-        res = requests.post(url=quest_url, params=params)
-        return res.json()
-    
-    def refresh_access_token(self,refresh_token):
-        '''obtain a new access token by sending a refresh token to the feedly Authorization server'''
-        params = dict(
-                      refresh_token=refresh_token,
-                      client_id=self.client_id,
-                      client_secret=self.client_secret,
-                      grant_type='refresh_token',
-                      )
-        quest_url=self._get_endpoint('v3/auth/token')
-        res = requests.post(url=quest_url, params=params)
-        return res.json()
-    
-    
-    def get_user_subscriptions(self,access_token):
-        '''return list of user subscriptions'''
-        headers = {'Authorization': 'OAuth '+access_token}
-        quest_url=self._get_endpoint('v3/subscriptions')
-        res = requests.get(url=quest_url, headers=headers)
-        return res.json()
-    
-    def get_feed_content(self,access_token,streamId,unreadOnly,newerThan):
-        '''return contents of a feed'''
-        headers = {'Authorization': 'OAuth '+access_token}
-        quest_url=self._get_endpoint('v3/streams/contents')
-        params = dict(
-                      streamId=streamId,
-                      unreadOnly=unreadOnly,
-                      newerThan=newerThan
-                      )
-        res = requests.get(url=quest_url, params=params,headers=headers)
-        return res.json()
-    
-    def mark_article_read(self, access_token, entryIds):
-        '''Mark one or multiple articles as read'''
-        headers = {'content-type': 'application/json',
-                   'Authorization': 'OAuth ' + access_token
-        }
-        quest_url = self._get_endpoint('v3/markers')
-        params = dict(
-                      action="markAsRead",
-                      type="entries",
-                      entryIds=entryIds,
-                      )
-        res = requests.post(url=quest_url, data=json.dumps(params), headers=headers)
-        return res
-    
-    def save_for_later(self, access_token, user_id, entryIds):
-        '''saved for later.entryIds is a list for entry id.'''
-        headers = {'content-type': 'application/json',
-                   'Authorization': 'OAuth ' + access_token
-        }
-        request_url = self._get_endpoint('v3/tags') + '/user%2F' + user_id + '%2Ftag%2Fglobal.saved'
-        
-        params = dict(
-                      entryIds=entryIds
-                      )
-        res = requests.put(url=request_url, data=json.dumps(params), headers=headers)
-        return res  	
+    def get_access_token(self, redirect_uri, code):
+        return self._make_request('POST', 'v3/auth/token', params={
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'grant_type': 'authorization_code',
+            'redirect_uri': redirect_uri,
+            'code': code
+        })
 
-    def _get_endpoint(self, path=None):
-        url = "https://%s" % (self.service_host)
-        if path is not None:
-            url += "/%s" % path
-        return url
+    def refresh_access_token(self, refresh_token):
+        return self._make_request('POST', 'v3/auth/token', params={
+            'refresh_token': refresh_token,
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'grant_type': 'refresh_token'
+        })
+    
+    def get_user_subscriptions(self):
+        return self._make_request('GET', 'v3/subscriptions')
+
+    def get_feed_content(self, streamId, unreadOnly=True, newerThan=None):
+        return self._make_request('GET', 'v3/streams/contents', params={
+            'streamId': streamId,
+            'unreadOnly': unreadOnly,
+            'newerThan': newerThan
+        })
+
+    def mark_article_read(self, entryIds):
+        return self._make_request('POST', 'v3/markers', data={
+            'action': "markAsRead",
+            'type': "entries",
+            'entryIds': entryIds
+        }, headers={'content-type': 'application/json'})
+
+    def save_for_later(self, user_id, entryIds):
+        request_url = self._get_endpoint('v3/tags') + f'/user%2F{user_id}%2Ftag%2Fglobal.saved'
+        return self._make_request('PUT', request_url, data={'entryIds': entryIds}, headers={'content-type': 'application/json'})
+    
+# Create a client instance
+client = FeedlyClient()
+
+# Get the user's subscriptions
+subscriptions = client.get_user_subscriptions()
+
+# Loop over the subscriptions
+for subscription in subscriptions:
+    # Get the id of the subscription
+    stream_id = subscription['id']
+
+    # Get the contents of the subscription
+    content = client.get_feed_content(stream_id)
+
+    # Print the title of each article in the feed
+    for item in content['items']:
+        print(item['title'])
